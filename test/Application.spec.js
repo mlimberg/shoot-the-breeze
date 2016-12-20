19c587bafbe1d7718ae107970998f14c162d4ca3
bekahlundy <lundyrebekah@gmail.com>
2016-12-20 16:59:18 -0700

    add more tests on search and input

--- test/Application.spec.js
+++ test/Application.spec.js
@@ -6,7 +6,10 @@ import { assert, expect } from 'chai';
 
 import Application from '../lib/components/Application';
 import Search from '../lib/components/Search';
+import MessageContainer from '../lib/components/MessageContainer';
+import Users from '../lib/components/Users';
 import Input from '../lib/components/Input';
+
 
 describe('Application', () => {
 
@@ -43,21 +46,71 @@ describe('Application', () => {
     const wrapper = shallow(<Application />)
     expect(wrapper.state().user).to.equal(null)
   })
+
   it('has a state of allUsers to null', () => {
     const wrapper = shallow(<Application />)
     expect(wrapper.state().allUsers).to.equal(null)
   })
+
   it('has a state of sortMsg to true', () => {
     const wrapper = shallow(<Application />)
     expect(wrapper.state().sortMsg).to.equal(true)
   })
+
   it('has a state of userSelected to null', () => {
     const wrapper = shallow(<Application />)
     expect(wrapper.state().userSelected).to.equal(null)
   })
 
-
 });
+
+describe('Search', () => {
+
+  it('renders as a <div>', () => {
+    const wrapper = shallow(<Search messages/>)
+    assert.equal(wrapper.type(), 'div');
+ });
+
+  it('should have an input', () => {
+    const wrapper = shallow(<Search messages/>)
+    expect(wrapper.find('input')).to.have.length(1)
+  })
+
+  it('should have an input class of .searchInput', () => {
+    const wrapper = shallow(<Search messages />)
+    expect(wrapper.find('.searchInput')).to.have.length(1);
+  })
+
+  it('should have a button', () => {
+    const wrapper = shallow(<Search messages />)
+    expect(wrapper.find("button")).to.have.length(1);
+  })
+
+  it('should have an button class of .sortButton', () => {
+    const wrapper = shallow(<Search messages />)
+    expect(wrapper.find('.sortButton')).to.have.length(1);
+  })
+});
+
+describe('MessageContainer', () => {
+
+  it.skip('renders as a <div>', () => {
+    const wrapper = shallow(<MessageContainer messages />)
+    assert.equal(wrapper.type(), 'div');
+  });
+
+})
+
+describe('Users', () => {
+
+  it('renders as a <p>', () => {
+    const wrapper = shallow(<Users />)
+    assert.equal(wrapper.type(), 'p');
+  });
+
+})
+
+
 
 describe('Input', () => {
 
@@ -65,6 +118,31 @@ describe('Input', () => {
     const wrapper = shallow(<Input />)
     assert.equal(wrapper.type(), 'div');
   });
+
+  it('should have an input', () => {
+    const wrapper = shallow(<Input />)
+    expect(wrapper.find("input")).to.have.length(1);
+  })
+
+  it('should have an input class of .messageInput', () => {
+    const wrapper = shallow(<Input />)
+    expect(wrapper.find('.messageInput')).to.have.length(1);
+  })
+
+  it('should have 2 buttons', () => {
+    const wrapper = shallow(<Input />)
+    expect(wrapper.find("button")).to.have.length(2);
+  })
+
+  it('should have an button class of .submitMsgBtn', () => {
+    const wrapper = shallow(<Input />)
+    expect(wrapper.find('.submitMsgBtn')).to.have.length(1);
+  })
+
+  it('should have an button class of .clearMsgBtn', () => {
+    const wrapper = shallow(<Input />)
+    expect(wrapper.find('.clearMsgBtn')).to.have.length(1);
+  })
 
 
 });
